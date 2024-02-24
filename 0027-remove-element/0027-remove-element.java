a4fcd6f622e188344d9134b9c4324fc961ead14b
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-02-24 18:53:30 +0900

    Time: 0 ms (100%), Space: 41.8 MB (51.61%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,7 +3,6 @@ class Solution {
         int cnt = nums.length;
         for(int i = nums.length-1; i>-1; i--){
             if(nums[i] == val){
-                System.out.println("the index is " + i);
                 for(int j = i; j<nums.length-1; j++)
                     nums[j] = nums[j+1];
                 cnt--;
