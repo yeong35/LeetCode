618d86dec4fa252664816bacaf36313e21e1e295
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-06-29 23:01:12 +0900

    Time: 5 ms (16.89%), Space: 47 MB (40.81%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -2,17 +2,38 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
         int[] result = new int[2];
+        int key;
+        int secondIndex;
 
         for(int i = 0; i<numbers.length; i++){
-            for(int j = i+1; j<numbers.length; j++){
-                if(numbers[i]+numbers[j] == target){
-                    result[0] = i+1;
-                    result[1] = j+1;
-                    return result;
-                }
+            result[0] = i+1;
+            key = target - numbers[i];
+
+            secondIndex = binarySearch(numbers, i+1, numbers.length-1, key);
+
+            if(secondIndex != -1){
+                result[1] = secondIndex+1;
+                break;
             }
+            
         }
 
         return result;
+    }
+
+    public static int binarySearch(int[] numbers, int low, int high, int key){
+        
+        if(high >= low){
+            int mid = low + (high-low)/2;
+
+            if(numbers[mid] == key)
+                return mid;
+            else if(numbers[mid] > key)
+                return binarySearch(numbers, low, mid - 1, key);
+            else
+                return binarySearch(numbers, mid + 1, high, key);
+        }
+
+        return -1;
     }
 }
