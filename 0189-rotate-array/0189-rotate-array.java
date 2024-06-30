c8a101fada2b45c40424bbb4882ccad903eb2c39
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-06-30 18:50:45 +0900

    Time: 1 ms (48.25%), Space: 57.4 MB (39.77%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] temp = nums.clone();
+
+        for(int i = 0; i<nums.length; i++){
+            nums[(i+k)%nums.length] = temp[i];
+        }
+    }
+}
