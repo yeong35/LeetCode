5318bca8440470ca4928b723bb2f37ecf13e1e7d
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-07-03 12:28:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minDifference(int[] nums) {
+
+        if (nums.length < 5)
+            return 0;
+
+        Arrays.sort(nums);
+        int sum = 0;
+        int head = 0;
+        int tail = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++)
+            sum += nums[i];
+
+        for (int i = 0; i < 3; i++) {
+            if (head >= tail)
+                return 0;
+            System.out.println(sum / (tail - head + 1));
+            if (Math.abs(nums[head] - sum / (tail - head + 1)) > Math.abs(nums[tail] - sum / (tail - head + 1))) {
+                sum -= nums[head];
+                head++;
+            } 
+            else {
+                sum -= nums[tail];
+                tail--;
+            }
+
+        }
+        return nums[tail] - nums[head];
+    }
+}
