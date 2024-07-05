6f60c2308bb68a1f1a31a363e53df1803b7b3f9d
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-07-05 21:52:30 +0900

    Time: 0 ms (100%), Space: 45 MB (8.62%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+        int[] result = new int[n*2];
+
+        for(int i = 0; i<n; i++){
+            result[i*2] = nums[i];
+            result[i*2+1] = nums[n+i];
+        }
+
+        return result;
+    }
+}
