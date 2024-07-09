f30ac473e85fa954d157a58f2cce08b0c0587cf3
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-07-09 15:39:00 +0900

    Time: 2 ms (82.61%), Space: 46.2 MB (14.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,11 +2,13 @@ class Solution {
     public void moveZeroes(int[] nums) {
         
         int write = 0;
+        int temp;
 
         for(int read = 0; read<nums.length; read++){
             if(nums[read] != 0){
+                temp = nums[write];
                 nums[write] = nums[read];
-                nums[read] = 0;
+                nums[read] = temp;
                 write++;
             }
         }
