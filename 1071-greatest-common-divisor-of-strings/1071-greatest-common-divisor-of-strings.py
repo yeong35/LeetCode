d94b3426230bda52a5125d98503c1f4ac195b12d
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2024-10-20 12:55:20 -0400

    Time: 7 ms (98.83%), Space: 17 MB (6.23%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -9,21 +9,25 @@ class Solution:
             else:
                 prefix += str1[i]
 
-        longer = ""
-        if len(str1) > len(str2):
-            longer = str1
-        else:
-            longer = str2
-
         prefix = "".join(prefix)
 
         for i in reversed(range(1, len(prefix)+1)):
             if len(str1)%i==0 and len(str2)%i==0:
                 start = 0
                 end = i
-                while start < len(longer):
-                    print("hihi", prefix[:i], longer[start:end])
-                    if longer[start:end] != prefix[:i]:
+                while start < len(str1):
+                    print("hihi1", prefix[:i], str1[start:end])
+                    if str1[start:end] != prefix[:i]:
+                        return ""
+                    else:
+                        start = end
+                        end = end+i
+
+                start = 0
+                end = i
+                while start < len(str2):
+                    print("hihi2", prefix[:i], str2[start:end])
+                    if str2[start:end] != prefix[:i]:
                         return ""
                     else:
                         start = end
