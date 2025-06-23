8d51533abd6118f34fa2d8e59ad9448ae66643d8
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-06-23 13:52:33 -0400

    Time: 7 ms (27.79%), Space: 18.7 MB (99.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,28 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         result = []
-        indegree = [0] * numCourses
         graph = [[] for _ in range(numCourses)]
-        queue = []
+        indegree = [0]*numCourses
 
         for a, b in prerequisites:
             graph[b].append(a)
             indegree[a] += 1
         
-
+        queue = []
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
-
+        
         while queue:
             curr = queue.pop(0)
             result.append(curr)
-
             for i in graph[curr]:
-                indegree[i]-=1
+                indegree[i]-= 1
+
                 if indegree[i] == 0:
                     queue.append(i)
-    
-        if len(result) == numCourses:
+
+        if numCourses == len(result):
             return result
         
         return []
-
