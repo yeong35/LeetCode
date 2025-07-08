092fd920350078f144bab2201c092526ac0764e6
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-07-08 11:05:15 -0400

    Time: 6742 ms (5.01%), Space: 23.4 MB (8.73%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,20 +1,15 @@
 class RecentCounter:
 
     def __init__(self):
-        self.que = []
+        self.storage = []
 
     def ping(self, t: int) -> int:
-        self.que.append(t)
-
-        idx = 0
-        for i in range(len(self.que)):
-            if self.que[i]>=t-3000:
-                idx = i
-                break
-
-        self.que = self.que[i:]
-
-        return len(self.que)
+        self.storage.append(t)
+        minimum = t - 3000
+        for i in range(len(self.storage)):
+            if self.storage[i] >= minimum:
+                return len(self.storage) - i
+        
 
 
 # Your RecentCounter object will be instantiated and called as such:
