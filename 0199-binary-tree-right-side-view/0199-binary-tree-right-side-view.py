4ebbc400105e704ae565f715263b8f44ccf78a5a
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-07-17 11:04:54 -0400

    Time: 0 ms (100%), Space: 17.9 MB (17.15%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,7 +8,6 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-
         result = []
         stack = [(root, 0)]
 
@@ -17,12 +16,10 @@ class Solution:
 
             if len(result) == level:
                 result.append(curr.val)
-
+            
             if curr.left:
                 stack.append((curr.left, level+1))
-
             if curr.right:
                 stack.append((curr.right, level+1))
 
-        
         return result
