99647de1ba3601da7ca5134afc898b8f5fbe7de7
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-07-26 11:59:07 -0400

    Time: 3 ms (81.72%), Space: 17.8 MB (71.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,41 +1,44 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        n, m = len(grid), len(grid[0])
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        queue = deque()
 
-        move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        queue = collections.deque()
         fresh = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    fresh+=1
-                elif grid[i][j] == 2:
-                    grid[i][j] = 0
-                    queue.append((i,j))
-        
         sec = 0
-        if fresh == 0:
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                elif grid[i][j] == 1:
+                    fresh +=1
+
+        if fresh==0:
             return sec
-        
+
+
         while queue:
             k = len(queue)
+
             for _ in range(k):
                 x, y = queue.popleft()
 
-                for i, j in move:
+                for i, j in moves:
                     i += x
                     j += y
 
-                    if (0<=i<m and 0<=j<n) and grid[i][j] != 0:
-                        if grid[i][j] == 1:
-                            fresh -= 1
-                            if fresh == 0:
-                                return sec+1
-                        grid[i][j] = 0
+                    if 0<=i<n and 0<=j<m and grid[i][j] == 1:
+                        fresh -= 1
+                        grid[i][j]=2
                         queue.append((i,j))
+                    
+                    if fresh==0:
+                        return sec+1
             
-            sec+=1
+            sec += 1
+
+
 
 
         return -1
