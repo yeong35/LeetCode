834846b4ceda4e04a34f89513487304e6fc8b94a
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-07-29 15:45:12 -0400

    Time: 162 ms (89.67%), Space: 42.3 MB (94%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,27 +1,25 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        undirected = [[] for i in range(n)]
-        directed = set(tuple(i) for i in connections)
+        graph = [[] for i in range(n)]
+        edges = {(i,j) for i,j in connections}
+
+        for i, j in connections:
+            graph[i].append(j)
+            graph[j].append(i)
+
         cnt = 0
-
         stack = [0]
-        visit = set()
-        visit.add(0)
-
-        for a, b in connections:
-            undirected[a].append(b)
-            undirected[b].append(a)
+        visit = {0}
 
         while stack:
             curr = stack.pop()
 
-            for i in undirected[curr]:
+            for i in graph[curr]:
                 if i not in visit:
                     stack.append(i)
                     visit.add(i)
 
-                    if (curr, i) in directed:
-                        cnt+=1
-        
-            
+                    if (curr, i) in edges:
+                        cnt += 1
+
         return cnt
