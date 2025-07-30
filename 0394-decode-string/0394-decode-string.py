87baab180c0f846b9c1db1e5b06775f7b3f31d44
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-07-30 13:15:27 -0400

    Time: 0 ms (100%), Space: 17.7 MB (55.34%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -3,21 +3,22 @@ class Solution:
         stack = []
 
         for i in s:
-            if i != ']':
-                stack.append(i)
-            else:
+            if i == ']':
                 temp = ""
                 while stack and stack[-1] != '[':
                     temp = stack.pop() + temp
-                
                 stack.pop()
 
                 n = ""
                 while stack and stack[-1].isdigit():
                     n = stack.pop()+n
-                if n != "":
-                    stack.append(int(n)*temp)
+                
+                if n != '':
+                    stack.append(temp * int(n))
                 else:
                     stack.append(temp)
-                
+
+            else:
+                stack.append(i)
+
         return "".join(stack)
