0fd61072cc02a0aef3c362c01896133a8c0f294d
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-01 10:33:47 -0400

    Time: 0 ms (100%), Space: 17.9 MB (26.76%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -6,36 +6,25 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        stack = [root1]
-        leaf1 = []
-        leaf2 = []
+        def dfs(root):
+            if not root:
+                return []
 
-        while stack:
-            curr = stack.pop()
+            leafs = [] 
+            stack = [root]
 
-            if curr.left is None and curr.right is None:
-                leaf1.append(curr.val)
-                continue
+            while stack:
+                curr = stack.pop()
 
-            if curr.left:
-                stack.append(curr.left)
+                if not curr.left and not curr.right:
+                    leafs.append(curr.val)
+                
+                if curr.left:
+                    stack.append(curr.left)
 
-            if curr.right:
-                stack.append(curr.right)
-
-        stack.append(root2)
-
-        while stack:
-            curr = stack.pop()
-
-            if curr.left is None and curr.right is None:
-                leaf2.append(curr.val)
-                continue
-
-            if curr.left:
-                stack.append(curr.left)
-
-            if curr.right:
-                stack.append(curr.right)
-
-        return leaf1 == leaf2
+                if curr.right:
+                    stack.append(curr.right)
+            
+            return leafs
+        
+        return dfs(root1) == dfs(root2)
