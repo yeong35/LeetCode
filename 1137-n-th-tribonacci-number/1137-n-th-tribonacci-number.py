41af1245a0620e404bf374fa87c7939ec23ce5bc
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-07 07:11:57 -0400

    Time: 0 ms (100%), Space: 17.9 MB (7.75%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -3,6 +3,6 @@ class Solution:
         arr = [0, 1, 1]
 
         for i in range(3, 38):
-            arr.append(arr[i-3]+arr[i-2]+arr[i-1])
+           arr.append(arr[i-3] + arr[i-2] + arr[i-1]) 
         
         return arr[n]
