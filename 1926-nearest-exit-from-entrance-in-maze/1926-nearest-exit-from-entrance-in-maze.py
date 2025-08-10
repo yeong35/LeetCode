23f5573195a708dc44e48d8fbd98e3cf8ced76d9
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-10 09:50:53 -0400

    Time: 67 ms (70.28%), Space: 19.1 MB (94.85%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -3,13 +3,13 @@ class Solution:
         moves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         maze[entrance[0]][entrance[1]] = "+"
-        stack = [entrance]
+        q = [entrance]
         cnt = 0
         
-        while stack:
-            k = len(stack)
+        while q:
+            k = len(q)
             for _ in range(k):
-                x, y = stack.pop(0)
+                x, y = q.pop(0)
 
                 for i, j in moves:
                     i += x
@@ -19,7 +19,7 @@ class Solution:
                         if i == 0 or j == 0 or i == len(maze)-1 or j == len(maze[0])-1:
                             return cnt+1
 
-                        stack.append([i,j])
+                        q.append([i,j])
                         maze[i][j] = '+'
             cnt += 1
 
