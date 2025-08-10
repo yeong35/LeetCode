17b3a3bfa39a223b71a3864618d9eeec2c61f620
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-10 09:49:57 -0400

    Time: 47 ms (95.46%), Space: 19.1 MB (79%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,26 +1,27 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        move = [[1, 0], [-1, 0], [0, -1], [0, 1]]
-
-        maze[entrance[0]][entrance[1]] = '+'
+        n, m = len(maze), len(maze[0])
+        sec = 0
+        maze[entrance[0]][entrance[1]] = "+"
         q = [entrance]
-        step = 0
+        moves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         while q:
-            n = len(q)
-            for _ in range(n):
-                r, c = q.pop(0)
+            k = len(q)
 
-                for i, j in move:
-                    i += r
-                    j += c
+            for _ in range(k):
+                x, y = q.pop(0)
 
-                    if 0<=i<len(maze) and 0<=j<len(maze[0]) and maze[i][j] == ".":
-                        if i==0 or i==len(maze)-1 or j==0 or j==len(maze[0])-1:
-                            return step+1
-                        q.append([i, j])
+                for i, j in moves:
+                    i += x
+                    j += y
+
+                    if 0<=i<n and 0<=j<m and maze[i][j] == '.':
+                        if i == n-1 or i == 0 or j==m-1 or j==0:
+                            return sec+1
                         maze[i][j] = '+'
-
-            step += 1
+                        q.append([i,j])
             
+            sec += 1
+
         return -1
