0398f373d5212fdb945c53e3bf827b21111e29b3
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-10 09:50:29 -0400

    Time: 74 ms (65.04%), Space: 19.1 MB (79%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,27 +1,28 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        n, m = len(maze), len(maze[0])
-        sec = 0
-        maze[entrance[0]][entrance[1]] = "+"
-        q = [entrance]
         moves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        while q:
-            k = len(q)
-
+        maze[entrance[0]][entrance[1]] = "+"
+        stack = [entrance]
+        cnt = 0
+        
+        while stack:
+            k = len(stack)
             for _ in range(k):
-                x, y = q.pop(0)
+                x, y = stack.pop(0)
 
                 for i, j in moves:
                     i += x
                     j += y
 
-                    if 0<=i<n and 0<=j<m and maze[i][j] == '.':
-                        if i == n-1 or i == 0 or j==m-1 or j==0:
-                            return sec+1
+                    if 0 <= i < len(maze) and 0 <= j < len(maze[0]) and maze[i][j] == '.':
+                        if i == 0 or j == 0 or i == len(maze)-1 or j == len(maze[0])-1:
+                            return cnt+1
+
+                        stack.append([i,j])
                         maze[i][j] = '+'
-                        q.append([i,j])
-            
-            sec += 1
+            cnt += 1
 
         return -1
+
+
