294f01fe2c1b58c69b6b72537208f5664ee59b58
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-12 07:09:14 -0400

    Time: 115 ms (5.05%), Space: 18.7 MB (22.63%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,24 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0, 1]
+        ans = []
 
-        if n < 2:
-            return ans[:n+1]
-        
-        power = 2
-        current = 0
+        for i in range(n+1):
+            ans.append(Counter(bin(i))['1'])
 
-        for i in range(2, n+1):
-            if power == current:
-                power *= 2
-                current = 0
-
-            if current == 0:
-                ans.append(1)
-            else:
-                ans.append(ans[power]+ans[i-power])
-            
-            current += 1
-
-        
         return ans
