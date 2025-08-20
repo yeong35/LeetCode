ee823652cef699a154650449a3c926280a736084
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-20 09:58:59 -0400

    Time: 0 ms (100%), Space: 17.6 MB (86.46%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,16 +1,15 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        result = []
-        def backtracking(idx, arr, k, n):
-            
-            if k == 0 and n==0:
-                result.append(list(arr))
+        ans = []
+        def backtracking(k, n, start, temp):
+            if k==0:
+                if n==0:
+                    ans.append(list(temp))
             else:
-                for i in range(idx, 10):
-                    if n-i >= 0:
-                        arr.append(i)
-                        backtracking(i+1, arr, k-1, n-i)
-                        arr.pop()
-        
-        backtracking(1, [], k, n)
-        return result
+                for i in range(start, 10):
+                    temp.append(i)
+                    backtracking(k-1, n-i, i+1, temp)
+                    temp.pop()
+
+        backtracking(k,n,1,[])
+        return ans
