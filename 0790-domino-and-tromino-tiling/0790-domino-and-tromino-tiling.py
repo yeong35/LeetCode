e3d37023d82ac8b7b0b680433c73a0a202b9c4ac
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-21 06:22:14 -0400

    Time: 4 ms (31.48%), Space: 18 MB (34.64%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,10 +1,17 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        mod = pow(10, 9)+7
+        if n < 3:
+            return n
+        elif n == 3:
+            return 5
+        
+        modulo = 10**9+7
 
         arr = [1, 2, 5]
 
-        for i in range(3, 1000):
-            arr.append((arr[i-1]*2+arr[i-3])%mod)
-        
-        return arr[n-1]
+        for i in range(3, n):
+            arr.append((arr[i-3]+arr[i-1]*2)%modulo)
+
+        return arr[-1]
+
+
