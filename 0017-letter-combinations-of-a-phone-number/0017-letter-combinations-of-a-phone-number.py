1ec6313d7ba3873c56026b90162c00ddb5846a81
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-24 14:21:30 -0400

    Time: 0 ms (100%), Space: 17.7 MB (69.12%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,19 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def backtracking(digits, temp):
-            if len(digits) == 0:
-                result.append(temp)
-                return
-            else:
-                for i in d[digits[0]]:
-                    backtracking(digits[1:], temp+i)
-        if digits == "":
+        if digits=='':
             return []
 
+        ans = []
+        h = {
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz'
+        }
 
-        d = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-
-        result = []
+        def backtracking(digits, comb):
+            if digits == '':
+               ans.append(comb)
+            else:
+                for i in h[digits[0]]:
+                    backtracking(digits[1:], comb+i)
         backtracking(digits, "")
-
-        return result
+        return ans
+            
