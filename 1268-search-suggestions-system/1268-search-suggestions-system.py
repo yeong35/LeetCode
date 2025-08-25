074125536cff25552424d707686b56033b027807
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-25 09:46:02 -0400

    Time: 7 ms (85.96%), Space: 20.5 MB (94.33%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,45 +1,17 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.words = []
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-    
-    def insert(self, word):
-        curr = self.root
-
-        for i in word:
-            if i not in curr.children:
-                curr.children[i] = TrieNode()
-            curr = curr.children[i]
-            curr.words.append(word)
-
-    def recommend(self, word):
-        temp = []
-
-        curr = self.root
-
-        for i in word:
-            if i not in curr.children:
-                return []
-            curr = curr.children[i]
-                
-        return curr.words[:3]
-
-
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        ans = []
-        trie = Trie()
         products.sort()
-        for product in products:
-            trie.insert(product)
-        
-        temp = ''
+        typing = ''
+        ans = []
+
         for i in searchWord:
-            temp += i
-            ans.append(trie.recommend(temp))
+            typing += i
+            idx = bisect_left(products, typing)
+
+            temp = []
+            for j in products[idx:idx+3]:
+                if j.startswith(typing):
+                    temp.append(j)
+            ans.append(temp)
 
         return ans
