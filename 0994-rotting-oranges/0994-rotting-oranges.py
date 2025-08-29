01ad3996a994691a97d7e2d896be4216c9dcb756
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-08-29 10:38:13 -0400

    Time: 3 ms (81.41%), Space: 18.1 MB (5.82%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,39 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
+        move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         fresh = 0
-        sec = 0
-        moves = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        q = []
+        minute = 0
+        que = []
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     fresh+=1
                 elif grid[i][j] == 2:
-                    q.append([i,j])
+                    que.append([i,j])
+                    grid[i][j] = 0
 
-        if fresh==0:
-            return 0
+        if fresh == 0:
+            return minute
 
-        while q:
-            k = len(q)
+        while que:
+            minute += 1
+            k = len(que)
+
             for _ in range(k):
-                x, y = q.pop(0)
-
-                for i, j in moves:
+                x, y = que.pop(0)
+                print(x,y)
+                for i, j in move:
                     i += x
                     j += y
 
-                    if 0 <= i < m and 0<= j < n:
+                    if 0<=i<m and 0<=j<n:
                         if grid[i][j] == 1:
-                            grid[i][j]=2
                             fresh -= 1
-                            q.append([i,j])
-                        
-                        if fresh == 0:
-                            return sec+1
-            
+                            grid[i][j] = 0
+                            que.append([i,j])
 
-            sec+=1
-        return -1
+                            if fresh == 0:
+                                return minute
+
+        return -1        
