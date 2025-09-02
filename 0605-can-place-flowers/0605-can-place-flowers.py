3848b21de7f676c850a813e8229cc5b7cde8017d
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-09-02 10:19:23 -0400

    Time: 9 ms (45.97%), Space: 18 MB (82.59%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -5,7 +5,5 @@ class Solution:
             if flowerbed[i] == 0 and (i-1 < 0 or flowerbed[i-1]==0) and (i+1 == len(flowerbed) or flowerbed[i+1]==0):
                 flowerbed[i] = 1
                 n-=1
-
-        print(flowerbed)
         
         return n <= 0
