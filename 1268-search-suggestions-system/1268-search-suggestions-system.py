749dee6ae1d9a08425e851dd429396aca2386a4a
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-09-10 10:14:38 -0400

    Time: 3 ms (96.94%), Space: 20.6 MB (75.4%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,17 +1,15 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
-        typing = ''
         ans = []
-
+        t = ""
         for i in searchWord:
-            typing += i
-            idx = bisect_left(products, typing)
-
+            t += i
+            idx = bisect_left(products, t)
+            
             temp = []
-            for j in products[idx:idx+3]:
-                if j.startswith(typing):
-                    temp.append(j)
+            for word in products[idx:idx+3]:
+                if word.startswith(t):
+                    temp.append(word)
             ans.append(temp)
-
         return ans
