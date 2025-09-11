863d762d8c3ee0ccebb8f806f298000bbad1348a
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2025-09-11 10:53:08 -0400

    Time: 339 ms (11.62%), Space: 66.9 MB (82.06%) - LeetHub

--- 2891-method-chaining/2891-method-chaining.py
+++ 2891-method-chaining/2891-method-chaining.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    return animals[animals['weight']>=100].sort_values(by='weight', ascending=False)[['name']]
+    return animals[animals['weight']>100].sort_values(by='weight', ascending=False)[['name']]
