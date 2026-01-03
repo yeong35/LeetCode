ae06103ab534be8e36adc06d315437faa345d32e
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2026-01-03 15:12:05 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -0,0 +1,28 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.board = dict()
+
+    def addScore(self, playerId: int, score: int) -> None:
+        self.board[playerId] = score
+
+    def top(self, K: int) -> int:
+        topK = []
+
+        for i in self.board:
+            heapq.heappush(topK, self.board[i])
+            if len(topK) > K:
+                heapq.heappop(topK)
+
+        return sum(topK)
+
+    def reset(self, playerId: int) -> None:
+        self.board[playerId] = 0
+        
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
