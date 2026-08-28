01b467aab57c93623220b1df7bf310a02985eca4
Eunyoung Bang <50111572+yeong35@users.noreply.github.com>
2026-08-28 02:00:24 -0400

    Time: 50 ms (23.46%), Space: 19.3 MB (13.12%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,23 +1,16 @@
-class Solution(object):
-    def minFlips(self, a, b, c):
-        """
-        :type a: int
-        :type b: int
-        :type c: int
-        :rtype: int
-        """
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
         cnt = 0
 
-        while a > 0 or b > 0 or c > 0:
-            if c % 2 == 0:
-                cnt = cnt + a%2 + b%2
+        while c>0 or a>0 or b>0:
+
+            if c%2 == 0:
+                cnt += a%2+b%2
             else:
-                if a%2 == 0 and b%2==0:
-                    cnt+=1
-            
-            a >>= 1
-            b >>= 1
-            c >>= 1
-
-
+                if a%2 == 0 and b%2 == 0:
+                    cnt += 1
+            a = a >> 1
+            b = b >> 1
+            c = c >> 1
+        
         return cnt
